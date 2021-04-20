88dd54a54adbc026ef3ffc8f213375f2b675d688
zakeri-o <omidzakeri22@gmail.com>
2021-04-20 17:52:33 +0430

    commited by omid

--- app/src/main/java/com/example/myapplication/BaseActivity.kt
+++ app/src/main/java/com/example/myapplication/BaseActivity.kt
@@ -1,10 +1,10 @@
 package com.example.myapplication
 
-import android.opengl.Visibility
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.ProgressBar
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
 class BaseActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,11 +12,15 @@ class BaseActivity : AppCompatActivity() {
         setContentView(R.layout.activity_base)
     }
 
-    fun showProgress(progress : ProgressBar){
+    fun showProgress(progress: ProgressBar) {
         progress.visibility = View.VISIBLE
     }
 
-    fun hideProgress(progress : ProgressBar){
+    fun hideProgress(progress: ProgressBar) {
         progress.visibility = View.VISIBLE
+    }
+
+    fun shoeToast(str: String) {
+        Toast.makeText(this, str, Toast.LENGTH_LONG).show()
     }
 }
